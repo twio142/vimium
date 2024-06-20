4cfa059a65bcb557660853695676de44e88ef9c2
twio142 <64556708+twio142@users.noreply.github.com>
2024-06-20 18:09:24 +0200

    find mode: support matching for selected text

--- background_scripts/commands.js
+++ background_scripts/commands.js
@@ -337,7 +337,7 @@ const Commands = {
       "vomnibarWithSelection",
       "vomnibarWithSelectionInNewTab",
     ],
-    findCommands: ["enterFindMode", "performFind", "performBackwardsFind"],
+    findCommands: ["enterFindMode", "performFind", "performBackwardsFind", "findSelected", "findSelectedBackwards"],
     historyNavigation: ["goBack", "goForward"],
     tabManipulation: [
       "createTab",
@@ -442,6 +442,8 @@ const defaultKeyMappings = {
   "/": "enterFindMode",
   "n": "performFind",
   "N": "performBackwardsFind",
+  "*": "findSelected",
+  "#": "findSelectedBackwards",
 
   // Vomnibar
   "o": "Vomnibar.activate",
@@ -533,6 +535,8 @@ const commandDescriptions = {
   enterFindMode: ["Enter find mode", { noRepeat: true }],
   performFind: ["Cycle forward to the next find match"],
   performBackwardsFind: ["Cycle backward to the previous find match"],
+  findSelected: ["Find the selected text"],
+  findSelectedBackwards: ["Find the selected text backwards"],
 
   goPrevious: ["Follow the link labeled previous or <", { noRepeat: true }],
   goNext: ["Follow the link labeled next or >", { noRepeat: true }],
