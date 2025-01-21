055387fc0d4bcdd0866733cbf2d1ba71a05059c3
twio142 <64556708+twio142@users.noreply.github.com>
2025-01-21 23:32:35 +0100

    feat(visual): change behavior of `enter`; press `O` to open vomnibar with selection

--- content_scripts/mode_visual.js
+++ content_scripts/mode_visual.js
@@ -318,11 +318,14 @@ class VisualMode extends KeyHandlerMode {
       _name: `${this.id}/enter/click`,
       // Yank on <Enter>.
       keypress: (event) => {
-        if (event.key === "Enter") {
-          if (!event.metaKey && !event.ctrlKey && !event.altKey && !event.shiftKey) {
-            this.yank();
-            return this.suppressEvent;
-          }
+        if (!event.metaKey && !event.ctrlKey && !event.altKey && !event.shiftKey && event.key === "Enter") {
+          this.shouldRetainSelectionOnExit = true;
+          this.exit();
+          return this.suppressEvent;
+        } else if (!event.metaKey && !event.ctrlKey && !event.altKey && event.code === "KeyO") {
+            this.shouldRetainSelectionOnExit = true;
+            this.exit();
+            return NormalModeCommands.vomnibarWithSelection();
         }
         return this.continueBubbling;
       },
